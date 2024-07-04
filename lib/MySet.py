b16fb80a503ade83a41f36127989382dd5aab9b7
Mohammad Khan <qcc2016@yahoo.com>
2024-07-04 17:58:29 -0400

    added functions

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,25 @@
 class MySet:
+    def __init__(self, elements=None):
+        self.dictionary = {}
+        if elements is not None:
+            for element in elements:
+                self.dictionary[element] = True  # Use dictionary keys to simulate set behavior.
 
-    pass
+    def add(self, element):
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        if element in self.dictionary:
+            del self.dictionary[element]
+
+    def has(self, element):
+        return element in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+        return f'MySet: {{{",".join(map(str, self.dictionary.keys()))}}}'
